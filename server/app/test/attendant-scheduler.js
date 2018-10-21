bef2ab87662b9318f01179601e0dd4f183ff4051
tiagostutz <tiagostutz@bb.com.br>
2018-10-21 08:42:03 -0200

    attendant renew working

--- server/app/test/attendant-scheduler.js
+++ server/app/test/attendant-scheduler.js
@@ -50,6 +50,45 @@ describe("Attendant Scheduler simple scenarios", () => {
     })
 
 
+
+    it("Should have 3 assistant ON-LINE registered by topic publish", (done) => {
+        attendatScheduler.start(() => {            
+            mqttProvider.init(process.env.MQTT_BROKER_HOST, process.env.MQTT_USERNAME, process.env.MQTT_PASSWORD, process.env.MQTT_BASE_TOPIC, (mqttClient) => {    
+                const attendantMock1 = {
+                    id: 1231,
+                    status: status.attendant.connection.online,
+                    activeSessions: []
+                }
+                const attendantMock2 = {
+                    id: 1232,
+                    status: status.attendant.connection.online,
+                    activeSessions: []
+                }
+                const attendantMock3 = {
+                    id: 1233,
+                    status: status.attendant.connection.online,
+                    activeSessions: []
+                }    
+                assert.equal(attendatScheduler.getOrderedOnlineAttendants().length, 0)
+
+                mqttClient.publish(topics.server.attendants.online, { attendantInfo: attendantMock1 })
+                mqttClient.publish(topics.server.attendants.online, { attendantInfo: attendantMock2 })
+                mqttClient.publish(topics.server.attendants.online, { attendantInfo: attendantMock3 })
+
+                setTimeout(() => {
+                    assert.equal(attendatScheduler.getOrderedOnlineAttendants().length, 3)
+                    
+                    attendatScheduler.db.delete(attendatScheduler.dbPrefix  + "/" + attendantMock1.id)
+                    attendatScheduler.db.delete(attendatScheduler.dbPrefix  + "/" + attendantMock2.id)
+                    attendatScheduler.db.delete(attendatScheduler.dbPrefix  + "/" + attendantMock3.id)
+                    done()
+                }, 150)
+                
+            })
+        })
+    })
+
+
     it("Should expire the assistant after registering it by topic publish", (done) => {
         const keepAliveTTL = 100
         attendatScheduler.start(() => {            
@@ -72,6 +111,47 @@ describe("Attendant Scheduler simple scenarios", () => {
                         done()
                     }, keepAliveTTL+100)
                 }, 50)
+                
+            })
+        }, keepAliveTTL)
+    }).timeout(2000)
+
+
+
+
+    it("Should expire one assistant and refresh another after registering it by topic publish", (done) => {
+        const keepAliveTTL = 200
+        attendatScheduler.start(() => {            
+            mqttProvider.init(process.env.MQTT_BROKER_HOST, process.env.MQTT_USERNAME, process.env.MQTT_PASSWORD, process.env.MQTT_BASE_TOPIC, (mqttClient) => {    
+                const attendantMock1 = {
+                    id: 41,
+                    status: status.attendant.connection.online,
+                    activeSessions: []
+                }    
+                const attendantMock2 = {
+                    id: 42,
+                    status: status.attendant.connection.online,
+                    activeSessions: []
+                }    
+                assert.equal(attendatScheduler.getOrderedOnlineAttendants().length, 0)
+
+                mqttClient.publish(topics.server.attendants.online, { attendantInfo: attendantMock1 })
+                mqttClient.publish(topics.server.attendants.online, { attendantInfo: attendantMock2 })
+
+                setTimeout(() => {
+                    assert.equal(attendatScheduler.getOrderedOnlineAttendants().length, 2)
+                    mqttClient.publish(topics.server.attendants.online, { attendantInfo: attendantMock2 })
+
+                    setTimeout(() => { //check if the assistant expired
+                        assert.equal(attendatScheduler.getOrderedOnlineAttendants().length, 1)
+                        assert.equal(attendatScheduler.getOrderedOnlineAttendants()[0].id, 42)
+                        
+                        attendatScheduler.db.delete(attendatScheduler.dbPrefix  + "/" + attendantMock1.id)
+                        attendatScheduler.db.delete(attendatScheduler.dbPrefix  + "/" + attendantMock2.id)
+                        done()
+                    }, keepAliveTTL-10)
+
+                }, keepAliveTTL/2)
                 
             })
         }, keepAliveTTL)
--- server/app/test/lib/database-provider.js
+++ server/app/test/lib/database-provider.js
@@ -66,4 +66,42 @@ describe("Tests DatabaseProvider", () => {
             done();
         }, 300)
     })
+
+
+    it("should update, merge, renew AND expire simple data", (done) => {
+        db.insert("/test3", { "objA": "testA", "objB": "testB" })
+        const obj = db.get("/test3")
+        
+        db.insert("/test3", { "objA": "testA2" }, false, 200)
+        const obj2 = db.get("/test3")
+
+        assert.equal(obj.objA, "testA")
+        assert.equal(obj.objB, "testB")
+        assert.equal(obj2.objA, "testA2")
+        assert.equal(obj2.objB, "testB")
+
+        const obj3 = db.get("/test3")
+        assert.equal(JSON.stringify(obj2), JSON.stringify(obj3))
+
+        // must be expired
+        setTimeout(() => {
+            const nullObj = db.get("/test3")
+            assert.equal(nullObj, null)
+            done();
+        }, 400)
+
+        // must be renewd
+        setTimeout(() => {
+            const obj3Alive = db.get("/test3")
+            assert.equal(JSON.stringify(obj3Alive), JSON.stringify(obj3))                
+        }, 200)
+
+        // renew
+        setTimeout(() => {
+            const obj3Renew = db.get("/test3")
+            assert.equal(JSON.stringify(obj3Renew), JSON.stringify(obj3))
+
+            db.insert("/test3", { "objA": "testA2" }, false, 200) 
+        }, 100)
+    })
 })
